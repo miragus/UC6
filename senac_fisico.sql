df1ed4c7cb7371a27450bbbd193ec41cee388580
Rhuan Gustavo Miranda <153655600+miragus@users.noreply.github.com>
2024-03-21 17:18:16 -0300

    21/03

---
+++ senac_fisico.sql
@@ -0,0 +1,147 @@
+create database senac_fisico;
+
+use senac_fisico;
+
+create table endereco(
+	id_endereco char (10) primary key,
+	rua varchar(50),
+	bairro varchar(50),
+	cidade varchar(50),
+	estado varchar(50),
+	cep char (8)
+);
+----------------------------------------------------------------------------------------------------------
+create table senac(
+	cnpj_senac char(14) primary key,
+	id_endereco char (10),
+	nome varchar(20),
+	telefone char(11),
+	sede varchar(50) UNIQUE,
+	foreign key	(id_endereco) references endereco(id_endereco)
+);
+----------------------------------------------------------------------------------------------------------
+create table curso(
+	id_curso char(10) primary key,
+	nome varchar(50),
+	duracao date,
+	modalidade bit,
+	area varchar(50)
+);
+----------------------------------------------------------------------------------------------------------
+create table vaga(
+	cnpj_senac char(14),
+	id_curso char (10),
+	foreign key (cnpj_senac) references senac(cnpj_senac),
+	foreign key (id_curso) references curso (id_curso)
+);
+----------------------------------------------------------------------------------------------------------
+create table tecnico(
+	id_curso char(10),
+	foreign key (id_curso) references curso (id_curso)
+);
+----------------------------------------------------------------------------------------------------------
+create table livre(
+	id_curso char(10),
+	foreign key (id_curso) references curso (id_curso)
+);
+----------------------------------------------------------------------------------------------------------
+create table empresa(
+	cnpj_empresa char(14) primary key,
+	id_endereco char(10),
+	telefone char(11),
+	email varchar(50),
+	nome varchar(50),
+	foreign key	(id_endereco) references endereco(id_endereco)
+);
+----------------------------------------------------------------------------------------------------------
+create table parceria(
+	cnpj_senac char(14),
+	cnpj_empresa char (14),
+	foreign key (cnpj_senac) references senac(cnpj_senac),
+	foreign key (cnpj_empresa) references empresa(cnpj_empresa)
+);
+----------------------------------------------------------------------------------------------------------
+create table funcionario(
+	matricula_f char(10) primary key,
+	id_endereco char(10),
+	cpf char(11),
+	email varchar(50),
+	nome varchar(50),
+	data_nascimento date,
+	telefone char(11),
+	foreign key	(id_endereco) references endereco(id_endereco)
+);
+----------------------------------------------------------------------------------------------------------
+create table professor(
+	matricula_f char(10) primary key,
+	graduacao varchar(50),
+	area varchar(50),
+	foreign key (matricula_f) references funcionario(matricula_f)
+);
+----------------------------------------------------------------------------------------------------------
+create table coordenador(
+	matricula_f char(10),
+	graduacao varchar(50),
+	foreign key (matricula_f) references funcionario(matricula_f)
+);
+----------------------------------------------------------------------------------------------------------
+create table atendente(
+	matricula_f char(10) primary key,
+	foreign key (matricula_f) references funcionario(matricula_f)
+);
+----------------------------------------------------------------------------------------------------------
+create table aluno(
+	matricula_a char(10) primary key,
+	cpf char(11),
+	nome varchar(50),
+	email varchar(50),
+	telefone char(11),
+	data_nascimento date,
+	id_endereco char(10),
+	foreign key	(id_endereco) references endereco(id_endereco)
+);
+----------------------------------------------------------------------------------------------------------
+create table atendimento(
+	matricula_a char(10),
+	matricula_f char(10),
+	foreign key (matricula_f) references atendente(matricula_f),
+	foreign key (matricula_a) references aluno(matricula_a)
+);
+----------------------------------------------------------------------------------------------------------
+create table turma(
+	matricula_a char(10),
+	matricula_f char(10),
+	foreign key (matricula_a) references aluno(matricula_a),
+	foreign key (matricula_f) references professor(matricula_f)
+);
+----------------------------------------------------------------------------------------------------------
+create table comercial(
+	matricula_a char(10),
+	valor decimal(6,2),
+	foreign key (matricula_a) references aluno(matricula_a)
+);
+----------------------------------------------------------------------------------------------------------
+create table bolsista(
+	matricula_a char(10),
+	financiamento decimal(5,2),
+	foreign key (matricula_a) references aluno(matricula_a)
+);
+----------------------------------------------------------------------------------------------------------
+create table psg(
+	matricula_a char(10),
+	foreign key (matricula_a) references aluno(matricula_a)
+);
+----------------------------------------------------------------------------------------------------------
+create table aprendiz(
+	matricula_a char(10) primary key,
+	cnpj_empresa char(14),
+	foreign key (matricula_a) references aluno(matricula_a),
+	foreign key (cnpj_empresa) references empresa(cnpj_empresa)
+);
+----------------------------------------------------------------------------------------------------------
+create table contrato(
+	matricula_a char(10),
+	cnpj_empresa char(14),
+	foreign key (matricula_a) references aprendiz(matricula_a),
+	foreign key (cnpj_empresa) references empresa(cnpj_empresa)
+);
